aa89b4686a6fa52bb8f40aba368a6e79cbbe32c8
Jose Septien <jaseptienh@gmail.com>
2016-10-25 21:34:44 -0500

    Add object describing my bio.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,8 +4,31 @@ console.log(awesomeThougths);
 funThoughts = awesomeThougths.replace("AWESOME", "FUN");
 $("#main").append(funThoughts);*/
 
-var formatterdName = HTMLheaderName.replace("%data%", "Jose Septien");
-var formatterdRole = HTMLheaderRole.replace("%data%", "Student and Programer");
+//var name = "Jose Septien";
+//var formatterdName = HTMLheaderName.replace("%data%", name);
+//var role = "Student and Programer";
+//var formatterdRole = HTMLheaderRole.replace("%data%", role);
 
-$("#main").prepend(formatterdRole);
-$("#main").prepend(formatterdName);
+/*$("#main").prepend(formatterdRole);
+$("#main").prepend(formatterdName);*/
+
+var bio = {
+    "name": "Jose Septien",
+    "role": "Student and Programer",
+    "contacts": {
+        "mobil": "473-738-5512",
+        "email": "jaseptienh@gmail.com",
+        "twitter": "@jaSeptienh"
+        "github": "Septien",
+        "location": "Guanajuato"
+    },
+    "picture": "./images/fry.jpg",
+    "welcomeMessage": "Welcome! This is my resume.",
+    "skills": [
+        "C/C++", "Pyhton", "JS", "Processing", "HTML/CSS."
+    ]
+};
+
+var formatterdName = HTMLheaderName.replace("%data%", bio.name);
+var formatterdRole = HTMLheaderRole.replace("%data%", bio.role);
+

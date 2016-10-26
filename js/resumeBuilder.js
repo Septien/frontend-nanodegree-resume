d1fa26aaec792c0c2614dd5919631dcd7bc54e8f
Jose Septien <jaseptienh@gmail.com>
2016-10-25 20:18:05 -0500

    Add role and name to the resume.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,11 @@
-var awesomeThougths = "I am Jose and I am AWESOME!";
+/*var awesomeThougths = "I am Jose and I am AWESOME!";
 console.log(awesomeThougths);
 
 funThoughts = awesomeThougths.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
+$("#main").append(funThoughts);*/
+
+var formatterdName = HTMLheaderName.replace("%data%", "Jose Septien");
+var formatterdRole = HTMLheaderRole.replace("%data%", "Student and Programer");
+
+$("#main").prepend(formatterdRole);
+$("#main").prepend(formatterdName);
